4678e9d2f4aca07cf966496c53d0ca7861138c03
MilosPusic2021 <92999387+MilosPusic2021@users.noreply.github.com>
2021-11-05 19:01:49 +0100

    Update Audit

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -10,3 +10,21 @@ Ucenik, Predmet, Nastavnik, Ocena, Datum FROM inserted)
 END
 
 INSERT INTO Ocena49 VALUES ('Muzurovic', 'Baze', 'Pusic', 3, '2021-11-1')
+
+CREATE TRIGGER Update_Ocena ON Ocena49
+AFTER UPDATE
+AS
+BEGIN
+INSERT INTO Ocena_Audit 
+(ID_Ocena, Akcija, Datum_Akcije, NewUcenik, OldUcenik, NewPredmet, OldPredmet, 
+NewNastavnik, OldNastavnik, NewOcena, OldOcena, NewDatum, OldDatum)
+(SELECT Ins.ID, 'Azuriranje', GETDATE(),
+Ins.Ucenik, Del.ucenik, 
+Ins.Predmet, Del.Predmet,
+Ins.Nastavnik, Del.Nastavnik,
+Ins.Ocena, Del.Ocena, 
+Ins.Datum, Del.Datum
+FROM inserted AS Ins 
+JOIN deleted AS Del ON Ins.ID = Del.ID)
+END
+

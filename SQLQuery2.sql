c1cb4b4754086508200f04c3930f111efe7a9310
MilosPusic2021 <92999387+MilosPusic2021@users.noreply.github.com>
2021-11-05 19:11:24 +0100

    Audit complete

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -28,3 +28,18 @@ FROM inserted AS Ins
 JOIN deleted AS Del ON Ins.ID = Del.ID)
 END
 
+UPDATE Ocena49 SET Ucenik = 'Petar M' WHERE ID=6
+
+DROP TRIGGER Delete_Ocena
+CREATE TRIGGER Delete_Ocena ON Ocena49
+AFTER DELETE
+AS
+BEGIN
+INSERT INTO Ocena_Audit
+(ID_Ocena, Akcija, Datum_Akcije, OldUcenik, OldPredmet, 
+OldNastavnik, OldOcena, OldDatum)
+(SELECT ID, 'Brisanje', GETDATE(),
+Ucenik, Predmet, Nastavnik, Ocena, Datum FROM deleted)
+END
+
+DELETE FROM Ocena49 WHERE ID=6
